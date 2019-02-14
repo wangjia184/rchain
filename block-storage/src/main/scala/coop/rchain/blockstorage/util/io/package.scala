21a8d89c5d4a853943e185805cd2bb37e057c7ae
Daniyar Itegulov <daniyar.itegulov@rchain.coop>
2019-02-14 12:19:38 +1100

    Introduce `Option` for distinguishing end-of-file errors from others

--- block-storage/src/main/scala/coop/rchain/blockstorage/util/io/package.scala
+++ block-storage/src/main/scala/coop/rchain/blockstorage/util/io/package.scala
@@ -39,6 +39,27 @@ package object io {
       }
     }
 
+  private[io] def handleIoF[F[_]: Sync: RaiseIOError, A](
+      io: => A,
+      handleIoException: IOException => F[A]
+  ): F[A] =
+    Sync[F].delay {
+      try {
+        io.pure[F]
+      } catch {
+        case e: IOException =>
+          handleIoException(e)
+        case e: SecurityException =>
+          RaiseIOError[F].raise[A](FileSecurityViolation(e))
+        case e: UnsupportedOperationException =>
+          RaiseIOError[F].raise[A](UnsupportedFileOperation(e))
+        case e: IllegalArgumentException =>
+          RaiseIOError[F].raise[A](IllegalFileOperation(e))
+        case e =>
+          RaiseIOError[F].raise[A](UnexpectedIOError(e))
+      }
+    }.flatten
+
   def moveFile[F[_]: Sync: RaiseIOError](
       from: Path,
       to: Path,
